3503c2f9a4ee78ceb04dfe6437db12092a4eff3a
duanyinglin1 <wobushidyl@gmail.com>
2023-10-08 16:41:45 +1100

    Add project files.

---
+++ Week9/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+
+[assembly: OwinStartupAttribute(typeof(Week9.Startup))]
+namespace Week9
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+      
+        }
+    }
+}
